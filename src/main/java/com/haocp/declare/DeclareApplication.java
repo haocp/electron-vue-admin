9bc1978eef1b5eea8f93d597132e4d537bfa5bc2
gms <haocp@163.com>
2021-01-22 11:18:08 +0800

    数据源切换 日志切片更新20210122

--- src/main/java/com/haocp/declare/DeclareApplication.java
+++ src/main/java/com/haocp/declare/DeclareApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 // 自定义数据源一定要排除SpringBoot自动配置数据源，不然会出现循环引用的问题
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+// 表示通过aop框架暴露该代理对象,AopContext能够访问
+@EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan(basePackages = "com.haocp.declare.web.mapper.*")
 public class DeclareApplication {
 
